fef0d566c49ffafa9409245b8379d45e21059624
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 00:53:47 +0200

    feature: Create models for Messages and Comments.     - Create two separate db models for Messaging on the wall feature and         Comments on those Messages.     - Register them(Message & Comment) to the admin panel.     - Make the migrations.

--- src/timeline/models.py
+++ src/timeline/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth.models import User
+import misaka
 
-# Create your models here.
+
+class Message(models.Model):
+    title          = models.CharField(max_length=150, unique=True)
+    slug           = models.SlugField(allow_unicode=True, blank=True, unique=True)
+    body           = models.TextField()
+    body_html      = models.TextField(editable=False)
+    date_created   = models.DateTimeField(default=timezone.now, verbose_name='date created')
+    date_published = models.DateTimeField(blank=True, null=True, verbose_name='date published')
+    author         = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering        = ['-date_published']
+        unique_together = ['author', 'slug']
+
+    def save(self, *args, **kwargs):
+        self.slug      = slugify(self.title)
+        self.body_html = misaka.html(self.body)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('timeline:single', kwargs={'slug': self.slug})
+
+    def publish(self):
+        self.date_published = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text             = models.TextField()
+    date_created     = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+    author           = models.ForeignKey(User, on_delete=models.CASCADE)
+    message          = models.ForeignKey('timeline.Message', related_name='comments', on_delete=models.CASCADE)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('timeline:list')
+
+    def __str__(self):
+        return f'{self.text[0:100]} ...'
+
