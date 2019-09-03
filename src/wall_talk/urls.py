0cf3f9aa7d6c6e9cbbb8946c9a31e92dc280c44f
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-03 19:07:36 +0200

    feature: Add a home page to the site.     - Create a base template page to be inhertited/extended.     - Create an index template page for the home view.     - Create a home-page-class view that renders the index page.     - Add a corresponding route for that view at the site's urlpatterns.

--- src/wall_talk/urls.py
+++ src/wall_talk/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views as site_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', site_views.HomePageView.as_view(), name='home'),
 ]
 
 if settings.DEBUG:
