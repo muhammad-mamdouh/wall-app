0f240a6e75ea9065bfc954608e0d91f8f96c205c
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 10:49:31 +0200

    feature: Add delete-message by its author feature.     - Create delete-message-confirmation template page.     - Create delete-message view class.     - Add corresponding route for it at the timeline app's urlpatterns.     - Update detail-message page with the deletion link.     - Update base template with flash-messages handler.

--- src/timeline/urls.py
+++ src/timeline/urls.py
@@ -3,6 +3,7 @@ from .views import (
     MessageListView,
     MessageDetailView,
     MessageCreateView,
+    MessageDeleteView,
 )
 
 
@@ -10,5 +11,6 @@ app_name    = 'timeline'
 urlpatterns = [
     path('messages/', MessageListView.as_view(), name='list'),
     path('message/new/', MessageCreateView.as_view(), name='create'),
+    path('message/<slug:slug>/remove/', MessageDeleteView.as_view(), name='remove'),
     path('message/<slug:slug>/', MessageDetailView.as_view(), name='single'),
 ]
--- src/timeline/views.py
+++ src/timeline/views.py
@@ -2,8 +2,11 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
+    DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Message
 from .forms import MessageForm
 
@@ -25,3 +28,12 @@ class MessageCreateView(LoginRequiredMixin, CreateView):
         self.object.author = self.request.user
         self.object.save()
         return super().form_valid(form)
+
+
+class MessageDeleteView(LoginRequiredMixin, DeleteView):
+    model       = Message
+    success_url =  reverse_lazy('timeline:list')
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, 'Message has been deleted successfully!')
+        return super().delete(*args, **kwargs)
