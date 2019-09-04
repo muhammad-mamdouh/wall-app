e7145bcb13b0041f01bd608df8800d2107837707
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 21:24:21 +0200

    feature: Add password-change feature.     - Create two template pages for password-change form and password-change-done.     - Update profile page with link to the password-change feature.     - Add two corresponding routes at the account app's urlpatterns for         password-change and password-change-done.

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 
@@ -9,4 +10,17 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
+
+    # Password Change Links
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done'),
+                                              template_name='account/password_change.html'),
+        name='password_change',
+    ),
+    path(
+        'password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'),
+         name='password_change_done'
+    ),
 ]
