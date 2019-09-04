689482a748b14bd155e75455e5deb7e8bfe04ba6
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 02:45:30 +0200

    feature: Show single message.     - Create message-detail template page to render a detailed message.     - Create message-detail view class.     - Add corresponding route for it at the timeline app's urls.     - Update any links to the message-detail route.

--- src/timeline/urls.py
+++ src/timeline/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MessageListView
+from .views import MessageListView, MessageDetailView
 
 
 app_name    = 'timeline'
 urlpatterns = [
     path('messages/', MessageListView.as_view(), name='list'),
+    path('message/<slug:slug>/', MessageDetailView.as_view(), name='single'),
 ]
--- src/timeline/views.py
+++ src/timeline/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import (
     ListView,
+    DetailView,
 )
 from .models import Message
 
 
 class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
     model = Message
