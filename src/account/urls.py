94bdfe57570dd7f2672c4df890df2406f355f4f6
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 23:51:55 +0200

    feature: Update registration process to send confirmation email before user activated.     - Create account-activation template page.     - Create profile model to determine if the e-mail is confirmed or not using (signals).     - Create token module to generate unique tokens.     - Update signup view class so we can send account activation details on email and         deactivate the created user after registration is completed.     - Create activate-account view class that will check -if the user exists? > token is valid?         and if it's true it'll activate the user.     - Add corresponding url for activation process at the account app's urlpatterns.

--- src/account/urls.py
+++ src/account/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name    = 'account'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
