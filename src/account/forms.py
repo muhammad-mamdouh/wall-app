f746ac92b6b0587019da3b4320204965bbb0f9d5
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-04 19:31:12 +0200

    feature: Add update-profile-info feature.     - Create user-update form class.     - Create profile template page to render that user-update form.     - Create profile view function to handle the info-update requests.     - Add coressponding route for that feature at the account app's urlpatterns.     - Add profile info tab at site nav-bar.

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class UserSignUpForm(UserCreationForm):
@@ -11,3 +12,29 @@ class UserSignUpForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = 'Display Name'
         self.fields['email'].label    = 'Email Address'
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_email(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            try:
+                user = User.objects.exclude(pk=self.instance.pk).get(email=email)
+            except User.DoesNotExist:
+                return email
+            raise forms.ValidationError(f'Email: {user.email} is already in use.')
+
+    def clean_username(self):
+        if self.is_valid():
+            username = self.cleaned_data['username']
+            try:
+                account = User.objects.exclude(pk=self.instance.pk).get(username=username)
+            except User.DoesNotExist:
+                return username
+            raise forms.ValidationError(f'Username: {account.username} is already in use.')
--- src/account/views.py
+++ src/account/views.py
@@ -1,9 +1,26 @@
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-from .forms import UserSignUpForm
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserSignUpForm, UserUpdateForm
 
 class SignUpView(CreateView):
     form_class    = UserSignUpForm
     success_url   = reverse_lazy('account:login')
     template_name = 'account/signup.html'
+
+
+@login_required()
+def profile(request):
+    if request.method == 'POST':
+        update_form = UserUpdateForm(request.POST, instance=request.user)
+
+        if update_form.is_valid():
+            update_form.save()
+            messages.success(request, 'Your account has been updated successfully!')
+            return redirect('account:profile')
+    else:
+        update_form = UserUpdateForm(instance=request.user)
+
+    return render(request, 'account/profile.html', {'update_form': update_form})
