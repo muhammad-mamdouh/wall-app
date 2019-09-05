98bf3155139c939d701c07a7987c89c2faa2a86e
Muhammad Mamdouh <mahammad.mamdouh@gmail.com>
2019-09-05 03:37:22 +0200

    feature: Enable create-message through messages-api.     - Create message-create view class that does the heavy object creation process.     - Add corresponding route for it at the api's urlpatterns.

--- src/timeline/api/urls.py
+++ src/timeline/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MessageAPIView, MessageAPIRUDView
+from .views import MessageAPIView, MessageCreateView, MessageAPIRUDView
 
 
 app_name = 'api-messages'
 urlpatterns = [
     path('messages/all/', MessageAPIView.as_view(), name='messages-list'),
+    path('message/new/', MessageCreateView.as_view(), name='message-create'),
     path('message/<slug:slug>/', MessageAPIRUDView.as_view(), name='message-rud'),
 ]
--- src/timeline/api/views.py
+++ src/timeline/api/views.py
@@ -1,4 +1,8 @@
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 from timeline.models import Message
 from .serializers import MessageSerializer
 
@@ -9,6 +13,17 @@ class MessageAPIView(ListAPIView):
     """
     queryset         = Message.objects.all().order_by('-date_created')
     serializer_class = MessageSerializer
+
+
+class MessageCreateView(CreateAPIView):
+    """
+    API endpoint that enables users to share new messages with others.
+    """
+    queryset         = Message.objects.all().order_by('-date_created')
+    serializer_class = MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class MessageAPIRUDView(RetrieveUpdateDestroyAPIView):
